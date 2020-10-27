c7af4b710a2d49e66ff098fbcb069f8b2d009cc1
AbrarNitk <abrar.nitk@gmail.com>
2020-10-27 16:46:38 +0530

    added handleMissingServletRequestParams

--- template/src/main/java/com/acko/template/constant/GlobalCustomExceptionHandler.java
+++ template/src/main/java/com/acko/template/constant/GlobalCustomExceptionHandler.java
@@ -1,11 +1,41 @@
 package com.acko.template.constant;
 
+import com.acko.template.response.error.ApiError;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
-public class GlobalCustomExceptionHandler {
+public class GlobalCustomExceptionHandler extends ResponseEntityExceptionHandler {
 
+    /**
+     * Handle MissingServletRequestParameterException. Triggered when a 'required' request parameter
+     * is missing.
+     *
+     * @param ex      MissingServletRequestParameterException
+     * @param headers
+     * @param status
+     * @param request
+     * @return
+     */
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(
+        MissingServletRequestParameterException ex, HttpHeaders headers,
+        HttpStatus status, WebRequest request
+    ) {
+        String error = ex.getParameterName() + " parameter is missing";
+        logger.error(error);
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError error) {
+        return new ResponseEntity<>(error, error.getStatus());
+    }
 }
