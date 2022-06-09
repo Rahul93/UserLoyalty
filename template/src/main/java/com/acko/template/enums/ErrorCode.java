a45ce8e491bcd9b13daa465bfe6b035e0e76176e
Pooja <pooja.singhal@acko.tech>
2022-06-09 20:43:45 +0530

    Made a template for a spring application

---
+++ template/src/main/java/com/acko/template/enums/ErrorCode.java
@@ -0,0 +1,50 @@
+package com.acko.template.enums;
+
+import com.acko.template.response.error.ApiError;
+import com.acko.template.response.error.ErrorObject;
+import com.acko.template.threadlocal.ThreadContext;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+  BAD_REQUEST_ERROR(ErrorGroup.BAD_REQUEST,"Bad Request","001"),
+  UNCATEGORIZED_ERROR(ErrorGroup.GENERAL_ERRORS, "Internal Server Error", "001");
+
+  private ErrorGroup group;
+  private String message;
+  private String code;
+
+  private static ErrorObject getErrorObject(final ErrorCode errorCode) {
+    return new ErrorObject(
+        errorCode.getGroup().getPrefix() + errorCode.getCode(), errorCode.getMessage());
+  }
+
+  public static ResponseEntity<Object> getErrorResponse(
+      final ErrorCode errorCode, final String errorMessage, boolean useErrorMessage) {
+
+    ErrorObject errorObject = ErrorCode.getErrorObject(errorCode);
+    // Using error message in case of internal api requests
+    if (useErrorMessage) {
+      errorObject.setMessage(errorMessage);
+    }
+
+    ApiError error =
+        ApiError.builder()
+            .success(false)
+            .traceId(
+                (ThreadContext.getRequestContext() == null
+                        || StringUtils.isEmpty(
+                            ThreadContext.getRequestContext().get().getTraceId()))
+                    ? null
+                    : ThreadContext.getRequestContext().get().getTraceId())
+            .error(errorObject)
+            .build();
+
+    return new ResponseEntity<>(error, errorCode.getGroup().getHttpStatus());
+  }
+}
